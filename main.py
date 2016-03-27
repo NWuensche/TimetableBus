c320949b779d020b406519ed671337afc9c5cb6d
Niklas Wuensche <s0020102@msx.tu-dresden.de>
2016-03-27 20:48:55 +0200

    add main loop to main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import Monitor
+import time
 
 def main():
-    Monitor.print_buses()
+    while True:
+        Monitor.print_buses()
+        time.sleep(30)
 
 if __name__ == '__main__':
     main()
