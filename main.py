b59fb2c7313f638602f2718783b3ad256c7f1cff
Niklas Wuensche <s0020102@msx.tu-dresden.de>
2016-04-06 07:01:49 +0000

    Change from python3 to python, rename main()

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import Monitor
 import time
 
-def main():
 
-    # show bus lines every 30 seconds
-    while True:
-        Monitor.print_buses()
-        time.sleep(30)
 
 if __name__ == '__main__':
     main()
