f126ca103947269d95225bec6ba173f68cbb6c35
Niklas Wuensche <s0020102@msx.tu-dresden.de>
2016-04-06 19:21:23 +0200

    make tabs to spaces, add start message

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app_secret = Sensitive.app_secret
 pushEvent = "message"
 pushMessage = "first"
 buffer = StringIO()
-c = pycurl.Curl() 
+c = pycurl.Curl()
 c.setopt(c.URL, 'https://api.instapush.im/v1/post')
 c.setopt(c.HTTPHEADER, ['x-instapush-appid: ' + app_ID,
 'x-instapush-appsecret: ' + app_secret,
@@ -45,38 +45,43 @@ c.setopt(c.WRITEFUNCTION, buffer.write)
 # uncomment to see the post that is sent
 #c.setopt(c.VERBOSE, True)
 def change_push_message(pushMessage):
-	json_fields['trackers']['message']=pushMessage
-	postfields = json.dumps(json_fields)
-	c.setopt(c.POSTFIELDS, postfields)
+    json_fields['trackers']['message']=pushMessage
+    postfields = json.dumps(json_fields)
+    c.setopt(c.POSTFIELDS, postfields)
 
 def do_what_user_wants():
-	user = api.login(Sensitive.e_mail, Sensitive.password)
-	input_str = user['full_name']
-	input_tokens = input_str.split()
-	if input_tokens[0] == 'bus':
-		pushMessage = ''
-		start = "CasparDavidFriedrichStraße" # default value
-		if len(input_tokens) > 1:
-			start = input_tokens[1]
-		rides = Timetable.get_buses(start)	
-		for ride in rides:
-			pushMessage += "{line}: {time}Min, ".format(line = ride[0], time = ride[2])
-		change_push_message(pushMessage)
-		c.perform()
-		print("Message sent!\n")
-		
+    user = api.login(Sensitive.e_mail, Sensitive.password)
+    input_str = user['full_name']
+    input_tokens = input_str.split()
+    if input_tokens[0] == 'bus':
+        pushMessage = ''
+        start = "CasparDavidFriedrichStraße" # default value
+        if len(input_tokens) > 1:
+            start = input_tokens[1]
+        rides = Timetable.get_buses(start)
+        for ride in rides:
+            pushMessage += "{line}: {time}Min, ".format(line = ride[0], time = ride[2])
+        change_push_message(pushMessage)
+        c.perform()
+        print("Message sent!\n")
+
+def start_message():
+    change_push_message("RasPi started!")
+    c.perform()
+
 
 # setup an indefinite loop that looks for the door to be opened / closed
 def main():
-	while True:
-		body= buffer.getvalue()
-		do_what_user_wants() # look for username 
-		# reset the buffer
-		buffer.truncate(0)
-		buffer.seek(0)
-		time.sleep(15)
-		
-	c.close()
+    start_message()
+    while True:
+        body= buffer.getvalue()
+        do_what_user_wants() # look for username
+        # reset the buffer
+        buffer.truncate(0)
+        buffer.seek(0)
+        time.sleep(15)
+
+    c.close()
 
 if __name__ == '__main__':
-	main()
+    main()
